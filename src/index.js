4dfaaa3cefc8500f3a3c20dd2a5a3a8f92e17198
Mayron Alves <mayron@devsquad.com>
2024-04-26 20:07:00 -0600

    installed autoupdater

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, Notification, BrowserWindow } = require('electron/main');
 const path = require('path');
 const url = require('url');
+const { autoUpdater, AppUpdater } = require('electron-updater');
+
+// Updater Flags
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
 
 let win;
 
@@ -14,22 +17,48 @@ function createWindow() {
         slashes: true
     }));
 
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
     win.on('closed', () => {
         win = null;
     });
 }
 
-app.on('ready', createWindow);
+function createNotification(title, body) {
+    new Notification({ title, body }).show();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (win === null) {
+            createWindow();
+        }
+    });
+}).then(() => {
+    autoUpdater.checkForUpdates();
+    createNotification('Hello World', 'Checking for updates...');
+});
+
+autoUpdater.on('update-available', (info) => {
+    createNotification('Hello World', 'Update available!');
+    let update = autoUpdater.downloadUpdate();
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    createNotification('Hello World', 'No update available :(');
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    createNotification('Hello World', 'Update downloaded.');
+});
+
+autoUpdater.on('error', (info) => {
+    createNotification('Hello World', info);
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
-app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
-})
